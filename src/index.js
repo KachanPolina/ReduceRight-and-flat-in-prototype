3108b01c039a2d34e8265764f737787fc4f3c485
Polina Kachan <kachan.polina8@gmail.com>
2026-04-29 16:33:24 +0300

    Add function flat()

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ function MyArray(...args) {
 }
 MyArray.prototype = new MyArrayProto();
 
+MyArray.isMyArray = function (obj) {
+	return obj instanceof MyArray;
+};
+
 function MyArrayProto() {
 	this.push = function () {
 		if (arguments) {
@@ -16,6 +20,66 @@ function MyArrayProto() {
 			}
 		}
 		return this.length;
+	};
+	this.pop = function () {
+		if (this.length === 0) return;
+		const lastItem = this[this.length - 1];
+		delete this[--this.length];
+		return lastItem;
+	};
+	this.forEach = function (callback) {
+		for (let i = 0; i < this.length; i++) {
+			callback(this[i], i, this);
+		}
+	};
+	this.some = function (callback) {
+		for (let i = 0; i < arr.length; i++) {
+			if (callback(this[i], i, this)) {
+				return true;
+			}
+		}
+		return fasle;
+	};
+	this.every = function (callback) {
+		for (let i = 0; i < arr.length; i++) {
+			if (!callback(this[i], i, this)) {
+				return fasle;
+			}
+		}
+		return true;
+	};
+	this.map = function (callback) {
+		const result = new MyArray();
+		for (let i = 0; i < this.length; i++) {
+			result.push(callback(this[i], i, this));
+		}
+		return result;
+	};
+	this.concat = function (...args) {
+		const res = new MyArray();
+		for (let i = 0; i < args.length; i++) {
+			if (Array.isArray(args[i])) {
+				res.push(...args[i]);
+			} else if (MyArray.isMyArray(args[i])) {
+				for (let j = 0; j < args[i].length; j++) {
+					res.push(args[i][j]);
+				}
+			} else {
+				res.push(args[i]);
+			}
+		}
+		return res;
+	};
+	this.reverse = function () {
+		const lengthMinusOne = this.length - 1;
+		// let temp;
+		for (let i = 0; i < Math.floor(this.length / 2); i++) {
+			// temp = this[i];
+			// this[i] = this[lengthMinusOne - i];
+			// this[lengthMinusOne - i] = temp;
+			[this[i], this[lengthMinusOne - i]] = [this[lengthMinusOne - i], this[i]];
+		}
+		return this;
 	};
 
 	this.reduceRight = function (callback, startValue) {
@@ -37,8 +101,39 @@ function MyArrayProto() {
 		}
 		return result;
 	};
+
+	this.flat = function (depth = 1) {
+		const result = new MyArray();
+		let resultIndex = 0;
+		for (let i = 0; i < this.length; i++) {
+			if (!MyArray.isMyArray(this[i]) || depth === 0 ) {
+				result[resultIndex++] = this[i];
+			} else {
+				const innerArray = this[i].flat(depth - 1);
+
+				for (let j = 0; j < innerArray.length; j++) {
+					result[resultIndex++] = innerArray[j];
+				}
+			}
+		}
+
+		result.length = resultIndex;
+		return result;
+	};
 }
 
-const myArr = new MyArray();
-const result = myArr.reduceRight((a, b) => a - b, 10);
-console.log(result);
+const innerInnerArray2 = new MyArray(2, 22, 2, 6);
+const innerInnerArray1 = new MyArray(
+	2,
+	5,
+	9,
+	innerInnerArray2,
+	innerInnerArray2,
+);
+const innerArray1 = new MyArray(3, 7, innerInnerArray1);
+const innerArray3 = new MyArray(2);
+const myArr = new MyArray(1, 2, innerArray1, 1, innerArray3);
+console.log(myArr);
+console.log(myArr.flat());
+console.log(myArr);
+
